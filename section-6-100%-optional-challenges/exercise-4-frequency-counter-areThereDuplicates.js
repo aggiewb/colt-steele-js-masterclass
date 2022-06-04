c1f35347dfa923dcb2695dcc5cdafc6a859d5d13
Aggie Wheeler <aggiewbdev@gmail.com>
2022-06-04 15:13:48 -0700

    Add solution to areThereDuplicates problem which uses a frequency counter

--- section-6-100%-optional-challenges/exercise-4-frequency-counter-areThereDuplicates.js
+++ section-6-100%-optional-challenges/exercise-4-frequency-counter-areThereDuplicates.js
@@ -1,3 +1,15 @@
 // Implement a function called, areThereDuplicates, which accepts a variable number of
 // arguments, and checks whether there are any duplicates among the arguments passed in.
 // This implementation will need to use a frequency counter pattern.
+
+function areThereDuplicates(...args) {
+    const argsCounter = {};
+    for (let i = 0; i < args.length; i++) {
+        if (argsCounter[args[i]]) {
+           return true; 
+        } else {
+           argsCounter[args[i]] = 1; 
+        }
+    }
+    return false;
+}
