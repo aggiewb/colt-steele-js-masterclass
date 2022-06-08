c757e5113037a8e3760575b2543ff771d660879a
Aggie Wheeler <aggiewbdev@gmail.com>
2022-06-08 14:26:55 -0700

    Add solution to minSubArrayLen using a sliding window

--- section-6-100%-optional-challenges/exercise-8-sliding-window-minSubArrayLen.js
+++ section-6-100%-optional-challenges/exercise-8-sliding-window-minSubArrayLen.js
@@ -6,3 +6,22 @@
 // function. If there isn't one, return 0 instead.
 
 // Time Complexity - O(N) : Space Complexity - O(1)
+
+function minSubArrayLen(nums, sum) {
+    let [total, i, j] = [0, 0, 0];
+    let minLen = nums.length + 1;
+
+    while (i < nums.length) {
+        if (total < sum && j < nums.length) {
+            total += nums[j];
+            j++;
+        } else if (total >= sum) {
+            total -= nums[i];
+            minLen = Math.min(minLen, j - i);
+            i++; 
+        } else if (j === nums.length) {
+            break;
+        }
+    }
+    return minLen > nums.length ? 0 : minLen;
+}
