611c98e4c47947b75a32d3166d64b90c742685e6
Aggie Wheeler <aggiewbdev@gmail.com>
2022-07-20 12:46:55 -0700

    Add implementation to quick sort, and update variable names for pivot-helper.js

--- section-16-quick-sort/quick-sort.js
+++ section-16-quick-sort/quick-sort.js
@@ -1,9 +1,19 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
-    const pivot = arr[start];
-    let swapIndex = start;
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        const pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex - 1);
+        // right
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
 
+function pivot(arr, start = 0, end = arr.length - 1) {
+    let swapIndex = start;
+    const pivot = arr[0];
     for (let i = 1; i <= end; i++) {
-        if (pivot > arr[i]) {
+        if (arr[i] < pivot) {
             swapIndex++;
             swap(arr, i, swapIndex);
         }
