0dbfbd5e7261c317e8396eb5dee7ef99e686abd9
Aggie Wheeler <aggiewbdev@gmail.com>
2022-08-23 10:47:28 -0700

    Add skeleton Node and Queue classes, with enqueue and dequeue Queue instance methods

--- section-21-stacks-and-queues/queue.js
+++ section-21-stacks-and-queues/queue.js
@@ -15,3 +15,27 @@ queue.unshift("third");
 queue.pop() // first
 queue.pop() // second
 queue.pop() // third
+
+// queue singly linked list implementation
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+
+    }
+
+    dequeue() {
+        
+    }
+}
